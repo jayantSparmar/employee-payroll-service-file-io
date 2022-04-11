b2eee7b196c2c4051669074fd9d605c9e78d5d18
jayant <jayantparmar420@gmail.com>
2022-04-11 18:49:39 +0530

    Added: method to read from the file UC6.

--- src/test/java/com/bridgelabz/PayrollTest.java
+++ src/test/java/com/bridgelabz/PayrollTest.java
@@ -1,5 +1,4 @@
 package com.bridgelabz;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,23 +6,24 @@ import java.util.Arrays;
 
 @SuppressWarnings("deprecation")
 public class PayrollTest {
-    @Test
-    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
-        EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, 25000.56, "Devnandan Kumar"),
-                new EmployeePayrollData(2, 15000.65, "Manoj Kumar"),
-                new EmployeePayrollData(3, 22000.67, "Abhishek sinha") };
-    };
+
     @Test
     public void given3EmployeesWhenWrittenToFileShouldMatchEmployeesEntries() {
-        EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, 25000.56, "Devnandan Kumar"),
-                new EmployeePayrollData(2, 15000.65, "Manoj Kumar"),
-                new EmployeePayrollData(3, 22000.67, "Abhishek sinha") };
+        EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, 25000.56, "Jayant Singh Parmar"),
+                new EmployeePayrollData(2, 15000.65, "Ashvini kanojiya"),
+                new EmployeePayrollData(3, 22000.67, "Shubham Pawar") };
         EmployeePayrollService EmployeePayrollService;
         EmployeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
         EmployeePayrollService.write(com.bridgelabz.EmployeePayrollService.IOService.FILE_IO);
         long entriesCount=EmployeePayrollService.countEntries(com.bridgelabz.EmployeePayrollService.IOService.FILE_IO);
         EmployeePayrollService.printData(com.bridgelabz.EmployeePayrollService.IOService.FILE_IO);
         Assertions.assertEquals(3, entriesCount);
+    }
+
+    @Test
+    public void givenFileOnReadingFromFileShouldMatchEmployeeCount() {
+        EmployeePayrollService payrollService=new EmployeePayrollService();
+        long entries=payrollService.readEmployeeData(EmployeePayrollService.IOService.FILE_IO);
     }
 
 }
